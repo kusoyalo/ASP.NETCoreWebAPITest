1c3247f84a17289de6ff5afe7ce9dfa7d072bcd5
陳映廷 <kusoyalo@yahoo.com.tw>
2024-01-23 12:00:31 +0800

    加入專案檔案。

---
+++ Controllers/KusoController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NETCoreWebAPITest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class KusoController : ControllerBase
+    {
+        [HttpPost(Name = "Kuso")]
+        public IEnumerable<Kuso> GetKuso()
+        {
+            return Enumerable.Range(1, 5).Select(data => new Kuso()).ToArray();
+        }
+    }
+}
---
+++ Kuso.cs
@@ -0,0 +1,8 @@
+﻿namespace ASP.NETCoreWebAPITest
+{
+    public class Kuso
+    {
+        public string Name => "kuso";
+        public int Age => 43;
+    }
+}
